b497b9d3e2e64644f1aee9f37f302ae5d266fa9d
thecoder-droid <thegabrielfrancis@gmail.com>
2020-09-01 18:36:20 -0400

    message

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -7,7 +7,7 @@ switch (window.location.hostname) {
     APIURL = "http://localhost:3000";
     break;
   // this is the deployed react application
-  case "foodie-friendzy.herokuapp.com":
+  case "foodie-friendzy.herokuapp.com/":
     // this is the full url of your deployed API
     APIURL = "https://foodie-friendz.herokuapp.com";
 }
